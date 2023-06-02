3405ae8f67d2d88562557d5d102c37a36113f605
Orion Reyes Galaviz <orionreyes@Orions-MacBook-Pro.local>
2023-06-02 00:44:26 -0500

    deleted some extra files

--- helpers/convert_data.py
+++
@@ -1,7 +0,0 @@
-from sklearn.preprocessing import LabelEncoder
-from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-import numpy as np
-
-from helpers.utilities import featuresdf
-
